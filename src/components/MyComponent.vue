a4cc289816705ff9b3d26cb352e386cb6149104d
Alex Yusypenko <o.yusipenko@gmail.com>
2021-06-24 20:52:25 +0300

    first commit

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="myCopmonent">My First components on Vue.js</div>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  props: { msg: String },
+};
+</script>
+
+<style scoped></style>
