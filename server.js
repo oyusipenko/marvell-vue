903541c7c8649c69e4d4b3206845bd1235e3d4c5
Alex Yusypenko <o.yusipenko@gmail.com>
2021-08-04 16:26:25 +0300

    build ford eploy

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.use(express.static("build"));
+app.use(express.static("dist"));
 app.listen(PORT);
